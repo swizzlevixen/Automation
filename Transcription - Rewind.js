b95230676dde636e0cdf4db8f9353b6cf93cab83
Mark Boszko <mboszko@mac.com>
2018-07-13 12:27:28 -0700

    Human-readable JXA version of Rewind.scpt

---
+++ Transcription - Rewind.js
@@ -0,0 +1,26 @@
+// Human-readable JXA version of Rewind.scpt
+
+// Checks if QuickTime Player is running,
+var appQuickTime = Application("QuickTime Player")
+appQuickTime.includeStandardAdditions = true
+
+// Check that there is a media document open
+if (appQuickTime.documents.length > 0) {
+    media_document = appQuickTime.documents[0]
+	if (media_document.rate() == -2.0) {
+		media_document.rate = -5.0
+	} else if (media_document.rate() == -5.0) {
+		media_document.rate = -10.0
+	} else {
+		media_document.rate = -2.0
+	}
+} else {
+    quicktimeNotReady()
+}
+
+function quicktimeNotReady() {
+	appQuickTime.displayAlert('QuickTime Player not ready', {
+	    message: 'Please open media file you would like to transcribe as the active window.'
+	})
+	console.log("Error: QuickTime Player not ready.")
+}
